c0d7a643d92f8e02222db8b8bee786af90138748
Zekun Zhang <zzekunn@gmail.com>
2023-12-04 21:13:45 -0500

    first commit

---
+++ src/main/java/com/zz/webflux/SpringBootWebFluxAssign4ZekunZhangApplication.java
@@ -0,0 +1,13 @@
+package com.zz.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebFluxAssign4ZekunZhangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebFluxAssign4ZekunZhangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zz/webflux/SpringBootWebFluxAssign4ZekunZhangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zz.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebFluxAssign4ZekunZhangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
